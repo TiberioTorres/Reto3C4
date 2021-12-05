e94f987812876bdb223d6d2840949b5dd230b7fc
PAOLABALLEN19 <ladi.ballen.mt@correo.usa.edu.co>
2021-12-04 19:13:53 -0500

    Admin usuarios borrar

--- src/main/resources/templates/js/AdminUsuarios.js
+++ src/main/resources/templates/js/AdminUsuarios.js
@@ -1,155 +1,154 @@
-
 function closeForm() {
-  $(".form-popup-bg").removeClass("is-visible");
+    $(".form-popup-bg").removeClass("is-visible");
 }
 
-function openForm (id) {
-  $(".form-popup-bg").addClass("is-visible");
-  $("#id_usr").val(id);
-}
-
-
-$(document).ready(function ($) {
-  /* Contact Form Interactions */ 
-  $("#btnOpenForm").on("click", function (event) {
-    event.preventDefault();
-
+function openForm(id) {
     $(".form-popup-bg").addClass("is-visible");
-  });
+    $("#id_usr").val(id);
+}
 
-  //close popup when clicking x or off popup
-  
-  $(".form-popup-bg").on("click", function (event) {
-    if (
-      $(event.target).is(".form-popup-bg") ||
-      $(event.target).is("#btnCloseForm")
-    ) {
-      event.preventDefault();
-      $(this).removeClass("is-visible");
-    }
-  });
+
+$(document).ready(function($) {
+    /* Contact Form Interactions */
+    $("#btnOpenForm").on("click", function(event) {
+        event.preventDefault();
+
+        $(".form-popup-bg").addClass("is-visible");
+    });
+
+    //close popup when clicking x or off popup
+
+    $(".form-popup-bg").on("click", function(event) {
+        if (
+            $(event.target).is(".form-popup-bg") ||
+            $(event.target).is("#btnCloseForm")
+        ) {
+            event.preventDefault();
+            $(this).removeClass("is-visible");
+        }
+    });
 });
 
 
 
-$("document").ready(function () {
-  getUsrs();
+$("document").ready(function() {
+    getUsrs();
 });
 
 function getUsrs() {
-  $.ajax({
-    url: "http://localhost:8084/api/user/all",
-    type: "GET",
-    datatype: "JSON",
-    contentType: "application/json",
-    success: function (response) {
-      console.log(response);
-      showUsrs(response);
-    },
-    error: function (jqXHR, textStatus, errorThrown) {},
-  });
+    $.ajax({
+        url: "http://localhost:8084/api/user/all",
+        type: "GET",
+        datatype: "JSON",
+        contentType: "application/json",
+        success: function(response) {
+            console.log(response);
+            showUsrs(response);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {},
+    });
 }
 
 function showUsrs(response) {
-  let table = "<table>";
-  for (i = 0; i < response.length; i++) {
-    table += "<tr>";
-    table += "<td>" + response[i].identification + "</td>";
-    table += "<td>" + response[i].name + "</td>";
-    table += "<td>" + response[i].address + "</td>";
-    table += "<td>" + response[i].cellPhone + "</td>";
-    table += "<td>" + response[i].email + "</td>";
-    table += "<td>" + response[i].password + "</td>";
-    table += "<td>" + response[i].zone + "</td>";
-    table += "<td>" + response[i].type + "</td>";
-    table += '<td><center><button onclick="getUsrById(' + response[i].id + ')">Actualizar</button><center></td>';
-    table += '<td><center><button onclick="deleteUsr(' + response[i].id + ')">Borrar</button><center></td>';
-    table += "</tr>";
+    let table = "<table>";
+    for (i = 0; i < response.length; i++) {
+        table += "<tr>";
+        table += "<td>" + response[i].identification + "</td>";
+        table += "<td>" + response[i].name + "</td>";
+        table += "<td>" + response[i].address + "</td>";
+        table += "<td>" + response[i].cellPhone + "</td>";
+        table += "<td>" + response[i].email + "</td>";
+        table += "<td>" + response[i].password + "</td>";
+        table += "<td>" + response[i].zone + "</td>";
+        table += "<td>" + response[i].type + "</td>";
+        table += '<td><center><button onclick="getUsrById(' + response[i].id + ')">Actualizar</button><center></td>';
+        table += '<td><center><button onclick="deleteUsr(' + response[i].id + ')">Borrar</button><center></td>';
+        table += "</tr>";
 
-  }
+    }
 
-  table += "</table>";
-  $("#cloneList tbody").html(table);
+    table += "</table>";
+    $("#cloneList tbody").html(table);
 }
 
 function getUsrById(id) {
-  jQuery.support.cors = true;
-  $.ajax({
-      dataType: 'json',
-      url: "http://localhost:8084/api/user/" + id,
-      type: 'GET',
-      success: function (response) {
-        console.log(response);
-        let usr= response;
-        $("#identificacion-usr").val(usr.identification);
-        $("#nombre-usr").val(usr.name); 
-        $("#direccion-usr").val(usr.address);  
-        $("#celular-usr").val(usr.cellPhone); 
-        $("#email-usr").val(usr.email); 
-        $("#contrasena-usr").val(usr.password); 
-        $("#zona-usr").val(usr.zone);
-        $("#tipo-usr").val(usr.type);
-        openForm(id);
-      }})
+    jQuery.support.cors = true;
+    $.ajax({
+        dataType: 'json',
+        url: "http://localhost:8084/api/user/" + id,
+        type: 'GET',
+        success: function(response) {
+            console.log(response);
+            let usr = response;
+            $("#identificacion-usr").val(usr.identification);
+            $("#nombre-usr").val(usr.name);
+            $("#direccion-usr").val(usr.address);
+            $("#celular-usr").val(usr.cellPhone);
+            $("#email-usr").val(usr.email);
+            $("#contrasena-usr").val(usr.password);
+            $("#zona-usr").val(usr.zone);
+            $("#tipo-usr").val(usr.type);
+            openForm(id);
+        }
+    })
 }
 
 function getJSONUpdate(id) {
-  return {
-    id: $("#id_usr").val(),
-    identification: $("#identificacion-usr").val(),
-    name: $("#nombre-usr").val(),
-    address: $("#direccion-usr").val(),
-    cellPhone: $("#celular-usr").val(),
-    email: $("#email-usr").val(),
-    password: $("#contrasena-usr").val(),
-    zone: $("#zona-usr").val(),
-    type: $("#tipo-usr").val(),
-  };
+    return {
+        id: $("#id_usr").val(),
+        identification: $("#identificacion-usr").val(),
+        name: $("#nombre-usr").val(),
+        address: $("#direccion-usr").val(),
+        cellPhone: $("#celular-usr").val(),
+        email: $("#email-usr").val(),
+        password: $("#contrasena-usr").val(),
+        zone: $("#zona-usr").val(),
+        type: $("#tipo-usr").val(),
+    };
 }
 
 $("#btn_post_update").click(function() {
-  let data = getJSONUpdate();
-  let url = "http://localhost:8084/api/user/update";
+    let data = getJSONUpdate();
+    let url = "http://localhost:8084/api/user/update";
 
-  // PETICION PUT (UPDATE)
-  $.when(jqueryPUT(url, data)).done(function() { // AJAX => ASINCRONO
-    alert("Se ha actualizado correctamente!");
-    closeForm();
+    // PETICION PUT (UPDATE)
+    $.when(jqueryPUT(url, data)).done(function() { // AJAX => ASINCRONO
+        alert("Se ha actualizado correctamente!");
+        closeForm();
 
-  });
+    });
 });
 
 function jqueryPUT(url, data) {
-  return $.ajax({
-    url: url,
-    type:"PUT",
-    datatype:"JSON",
-    data: JSON.stringify(data),
-    contentType: "application/json"
-  });
+    return $.ajax({
+        url: url,
+        type: "PUT",
+        datatype: "JSON",
+        data: JSON.stringify(data),
+        contentType: "application/json"
+    });
 }
 
-function traerDatosUpdate(){
-  return [$()];
+function traerDatosUpdate() {
+    return [$()];
 }
 
 
-function updateUsr(id){
+function updateUsr(id) {
 
-    if ($("#identificacion-usr").val().length == 0 || 
-        $("#nombre-usr").val().length == 0 || 
-        $("#direccion-usr").val().length == 0 || 
-        $("#celular-usr").val().length==0||  
-        $("#email-usr").val().length == 0 || 
-        $("#contrasena-usr").val().length == 0 || 
-        $("#zona-usr").val().length == 0 || 
-        $("#tipo-usr").val().length== 0) {
+    if ($("#identificacion-usr").val().length == 0 ||
+        $("#nombre-usr").val().length == 0 ||
+        $("#direccion-usr").val().length == 0 ||
+        $("#celular-usr").val().length == 0 ||
+        $("#email-usr").val().length == 0 ||
+        $("#contrasena-usr").val().length == 0 ||
+        $("#zona-usr").val().length == 0 ||
+        $("#tipo-usr").val().length == 0) {
         alert("Por favor llene todos los campos de registro solicitados.");
-    }
-    else{
+    } else {
 
         let usr = {
-            id:id,
+            id: id,
             identification: $("#identificacion-usr").val(),
             name: $("#nombre-usr").val(),
             address: $("#direccion-usr").val(),
@@ -157,29 +156,29 @@ function updateUsr(id){
             email: $("#email-usr").val(),
             password: $("#contrasena-usr").val(),
             zone: $("#zona-usr").val(),
-            type: $("#tipo-usr").val()    
+            type: $("#tipo-usr").val()
         }
         console.log(usr.id);
-        let dataToSend=JSON.stringify(usr);
+        let dataToSend = JSON.stringify(usr);
 
         $.ajax({
-            url:"http://localhost:8084/api/user/update",
-            type:"PUT",
-            datatype:"JSON",
-            data:dataToSend,
+            url: "http://localhost:8084/api/user/update",
+            type: "PUT",
+            datatype: "JSON",
+            data: dataToSend,
             contentType: "application/json",
-            success: function(response){
+            success: function(response) {
                 console.log(response);
                 $("#usrList").empty();
                 getUsrs();
                 console.log("Se ha actualizado el registro del producto.");
                 alert("Se ha actualizado el registro del producto.")
             },
-            error: function(jqXHR, textStatus, errorThrown){
+            error: function(jqXHR, textStatus, errorThrown) {
                 console.log("No se ha actualizado el registro del usuario, por favor verifique.");
                 alert("No se ha actualizado el registro del usuario, por favor verifique.")
             }
-        });          
+        });
         console.log(response);
     }
 }
@@ -245,32 +244,32 @@ function saveProduct(){
 }
 */
 
-function deleteUsr(id){
+function deleteUsr(id) {
 
-  let product = {
-      id:id
-  };
+    let usr = {
+        id: id
+    };
 
-  console.log(product);
-  let dataToSend=JSON.stringify(product);
-  
-  $.ajax({
-      url: "http://localhost:8084/api/clone/"+id,
-      type:"DELETE",
-      datatype:"JSON",
-      data: dataToSend,
-      contentType: "application/json",
-      success: function(response){
-          console.log(response);
-          $("#productList").empty();
-          getUsrs();
-          console.log("Se ha eliminado el registro del producto.");
-          alert("Se ha eliminado el registro del producto.")
-      },
-      error: function(jqXHR, textStatus, errorThrown){
-          console.log("No se ha eliminado el registro del producto, por favor verifique.");
-          alert("Se ha eliminado el registro del producto, por favor verifique.")
-      }
-  });   
+    console.log(usr);
+    let dataToSend = JSON.stringify(usr);
+
+    $.ajax({
+        url: "http://localhost:8084/api/user/" + id,
+        type: "DELETE",
+        datatype: "JSON",
+        data: dataToSend,
+        contentType: "application/json",
+        success: function(response) {
+            console.log(response);
+            $("#usrList").empty();
+            getUsrs();
+            console.log("Se ha eliminado el registro del Usuario.");
+            alert("Se ha eliminado el registro del usuario.")
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log("No se ha eliminado el registro del usuario, por favor verifique.");
+            alert("No se ha eliminado el registro del usuario, por favor verifique.")
+        }
+    });
 
 }
